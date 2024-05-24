943468844088e6e5781bb935ecc008ec4e3f004e
yadamjoshna <yadamjoshna@gmail.com>
2024-05-24 14:40:12 +0530

    wrote hello world program and used static function

--- StaticMethod.java
+++ StaticMethod.java
@@ -1,10 +1,15 @@
 public class StaticMethod {
     public static void main(String[] args) {
-        System.out.println("static method");
         StaticMethod.method1();
+        System.out.println("static method");
     }
     static void method1(){
         System.out.println("Method 1");
     }
+    static {
+        System.out.println("static block");
+        StaticMethod.method1();
+    }
+
 }
 
