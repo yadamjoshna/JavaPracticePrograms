9de4325629424e5d0cf43864fb5f94ea1af204aa
yadamjoshna <yadamjoshna@gmail.com>
2024-05-24 14:20:38 +0530

    wrote hello world program and used static function

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+    static {
+        System.out.println("live love laugh");
+    }
+
+    static {
+        System.out.println("live love laugh like");
+    }
+}
