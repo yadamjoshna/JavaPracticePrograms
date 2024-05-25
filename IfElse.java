3f7e0a03dc5895fd168b6851e783cd4285e79f6b
yadamjoshna <yadamjoshna@gmail.com>
2024-05-25 16:37:29 +0530

    program using if else condition

---
+++ IfElse.java
@@ -0,0 +1,16 @@
+public class IfElse {
+    public static void main(String[] args) {
+        int a = 20000, b = 200;
+
+        if(a>b)
+        {
+            System.out.println("a is greater");
+        }
+        else if(a<b){
+            System.out.println("b is greater");
+        }
+        else {
+            System.out.println("both are same");
+        }
+    }
+}
