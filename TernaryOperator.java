484970aeb51b039873b11ef3f6e46b45621b6713
yadamjoshna <yadamjoshna@gmail.com>
2024-05-25 14:18:57 +0530

    program using ternary operator

--- TernaryOperator.java
+++ TernaryOperator.java
@@ -1,7 +1,7 @@
 public class TernaryOperator {
     public static void main(String[] args) {
-        int a= 10, b= 20;
-        String result = (a>b)?"a is greater":"b is greater";
+        int a= 29, b= 20;
+        String result = (a>b)?"you are mad":"i am mad";
         System.out.println(result);
     }
 }
